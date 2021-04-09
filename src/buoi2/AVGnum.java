b981d23b4dd7fc086e8e618b9bbf1061093ac63d
DESKTOP-MNA716V\Admin <tuanlc8899@gmail.com>
2021-04-09 17:09:27 +0700

    edit

--- src/buoi2/AVGnum.java
+++ src/buoi2/AVGnum.java
@@ -8,21 +8,27 @@ public class AVGnum {
 		// TODO Auto-generated method stub
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
+
 		
+		//Nhap vao so luong so
 		System.out.println("Nhap vao bao nhieu so? Nhap vao day: ");
 		int clientNumber = sc.nextInt();
 		
+		//Khoi tao tong cac so = 0
 		int clientSumNum = 0;
-		int dem = 0;
+		
+		//Duyet vong lap
 		for(int i=1; i<=clientNumber; i++)
 		{
 			System.out.println("Nhap vao so thu " + i);
 			clientSumNum += sc.nextInt();
-			dem ++;
 		}
-		float AVGnum = (float)(clientSumNum/dem);
-		System.out.print("Trung binh cong cua cac so vua nhap la: ");
-		System.out.println(AVGnum);
+		
+		//Trung binh cong cac so vua nhap
+		float AVGnum = (float)clientSumNum/clientNumber;
+		
+		//In ket qua
+		System.out.println("Trung binh cong cua cac so vua nhap la: " + AVGnum);
 		
 	}
 
---
+++ src/buoi2/LearnConstant.java
@@ -0,0 +1,14 @@
+package buoi2;
+
+public class LearnConstant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int MY_BIRTH = 1999;
+		System.out.println(MY_BIRTH);
+		
+		char x = 'a';
+		System.out.println(x);
+	}
+
+}
