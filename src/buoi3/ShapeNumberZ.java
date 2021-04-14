6c920001e60f6ef71cbb52e98bfe15ed726594af
DESKTOP-MNA716V\Admin <tuanlc8899@gmail.com>
2021-04-14 13:43:40 +0700

    JavaBase

--- src/buoi3/ShapeNumberZ.java
+++ src/buoi3/ShapeNumberZ.java
@@ -7,7 +7,7 @@ public class ShapeNumberZ {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/*
-		                    DOC 	NGANG	
+		                    DOC     NGANG	
 		                    7       7
 		 1 2 3 4 5 6 7              full
 		   2                        2
